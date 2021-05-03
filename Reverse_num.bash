39280f9f953feda3753dc2d38d7e21c1622a7d15
reshma-as <64767829+reshma-as@users.noreply.github.com>
2021-05-02 19:14:05 -1200

    Delete Reverse_num.bash

--- Reverse_num.bash
+++
@@ -1,9 +0,0 @@
-# script to find the reverse of a number
-read -p "Enter a number: " number
-temp=$number
-while [ $temp -ne 0 ]
-do
-    reverse=$reverse$((temp%10))
-    temp=$((temp/10))
-done
-echo "Reverse of $number is $reverse"
