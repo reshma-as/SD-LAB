e62711dfde0cc34ad6e6cc2c3dfcbc77c6b4f2b0
reshma-as <64767829+reshma-as@users.noreply.github.com>
2021-05-10 17:57:19 -1200

    Update Linearsearch.bash

--- Linearsearch.bash
+++ Linearsearch.bash
@@ -6,14 +6,14 @@
 # 
 # 
 #shell script for linear search
-echo Enter the number of elements:
+echo "Enter the number of elements:"
 read n
-echo Enter the array elements:
+echo "Enter the array elements:"
 for (( i=1; i<=n; i++ ))
 do
     read a[$i]
 done
-echo Enter the element to be searched:
+echo "Enter the element to be searched:"
 read item
 j=1
 while [ $j -lt $n -a $item -ne ${a[$j]} ]
@@ -22,7 +22,7 @@ do
 done
 if [ $item -eq ${a[$j]} ]
 then
-    echo $item is present at location $j
+    echo "$item is present at location $j"
 else
-    echo "$item is not present in the array."
+    echo "$item is not present in the array"
 fi
