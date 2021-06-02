308cc6a9a6798d7bf2150a99248c73948bbe0071
reshma-as <64767829+reshma-as@users.noreply.github.com>
2021-06-02 12:08:28 +0530

    Add files via upload

---
+++ Countloginuser.bash
@@ -0,0 +1,12 @@
+# 
+# Welcome to GDB Online.
+# GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+# C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+# Code, Compile, Run and Debug online from anywhere in world.
+# 
+# 
+cat /etc/passwd>user.txt
+        set `wc -l user.txt`
+        log=`who|wc -l`
+    echo "There are $1 users in network "
+    echo "There are $log user loged in rite now"  
